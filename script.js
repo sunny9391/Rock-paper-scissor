7219d14372c28cdcfb9028ddac86f284606fd2c7
soma shekar nadipineni <155898420+sunny9391@users.noreply.github.com>
2024-09-17 19:19:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,54 @@
-const choices=document.querySelectorAll(".select");
-const message=document.querySelector("#msg");
+const choices = document.querySelectorAll(".select");
+const message = document.querySelector("#msg");
 
-let userscore=0;
-let compscore=0;
+let userscore = 0;
+let compscore = 0;
 
-const userpara=document.querySelector("#uscore");
+const userpara = document.querySelector("#uscore");
+const comppara = document.querySelector("#cscore");
 
-const comppara=document.querySelector("#cscore");
+const genComp = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
+
+const drawgame = () => {
+  message.innerText = "Game was a Draw. Play again.";
+  message.style.backgroundColor = "#081b31";
+};
+
+const showwinner = (userwin, userchoice, compchoice) => {
+  if (userwin) {
+    userscore++;
+    userpara.innerText = userscore;
+    message.innerText = `You win! Your ${userchoice} beats ${compchoice}`;
+    message.style.backgroundColor = "green";
+  } else {
+    compscore++;
+    comppara.innerText = compscore;
+    message.innerText = `You lost! ${compchoice} beats your ${userchoice}`;
+    message.style.backgroundColor = "red";
+  }
+};
+
+const playGame = (userchoice) => {
+  const compchoice = genComp();
+
+  if (userchoice === compchoice) {
+    drawgame();
+  } else {
+    const userwin = (userchoice === "rock" && compchoice === "scissors") ||
+                    (userchoice === "paper" && compchoice === "rock") ||
+                    (userchoice === "scissors" && compchoice === "paper");
+
+    showwinner(userwin, userchoice, compchoice);
+  }
+};
 
 choices.forEach((select) => {
-    select.addEventListener("click", () => {
-      const userchoice = select.getAttribute("id");
-      playGame(userchoice);
-    });
+  select.addEventListener("click", () => {
+    const userchoice = select.getAttribute("id");
+    playGame(userchoice);
   });
-
-const genComp=()=>{
-   const obj=["ROCK","PAPER","SCISSOR"];
-   const idx=Math.floor(Math.random()*3);
-   return obj[idx];
-}
- 
-const drawgame=()=>{
-    message.innerHTML="IT WAS A DRAW";
-}
-
-const playGame=(userchoice)=>{
-
-    const compchoice=genComp();
-
-    if(compchoice==userchoice)
-    {
-        drawgame();
-    }
-    else
-    {
-        let userwin=true;
-        if(userchoice=="ROCK")
-        {
-            userwin=compchoice=="PAPER"?false:true;
-        }
-        else if(userchoice=="PAPER")
-        {
-            userwin=compchoice=="ROCK"?true:false;
-        }
-        else
-        {
-            userwin=compchoice=="PAPER"?true:false;
-        }
-       showwinner(userwin,userchoice,compchoice);
-    }
-
-} 
-
-const showwinner=(userwin,userchoice,compchoice)=>{
-    if(userwin)
-    {
-        userscore++;
-        userpara.innerHTML=userscore;
-        message.innerHTML=`You win! Your ${userchoice} beats ${compchoice}`;
-        message.style.backgroundColor='green';
-    }
-    else
-    {
-        compscore++;
-        comppara.innerHTML=compscore;
-        message.innerHTML=`You lost! Your ${userchoice} beats ${compchoice}`;
-        message.style.backgroundColor='red';
-    }
-}
+});
